d15ecec6e9c87be9f818684e81fe3c91b3b14f20
Samar-dot188 <gosaliasamar@gmail.com>
2025-07-31 19:44:38 +0530

    Update connectDB.php

--- public/connectDB.php
+++ public/connectDB.php
@@ -1,9 +1,9 @@
 <?php
 /* Database connection settings */
-	$servername = "localhost";
-    $username = "root";		//put your phpmyadmin username.(default is "root")
-    $password = "";			//if your phpmyadmin has a password put it here.(default is "root")
-    $dbname = "biometricattendace";
+	$servername = "sql211.infinityfree.com";
+    $username = "if0_39564165";		//put your phpmyadmin username.(default is "root")
+    $password = "Samhridh2011";			//if your phpmyadmin has a password put it here.(default is "root")
+    $dbname = "if0_39564165_biometricattendance";
     
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 	
