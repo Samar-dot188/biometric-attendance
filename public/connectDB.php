3a4b508ddc42928b21a19780825fbe14e8e14ead
Samar-dot188 <gosaliasamar@gmail.com>
2025-08-10 10:32:39 +0530

    Update connectDB.php

--- public/connectDB.php
+++ public/connectDB.php
@@ -1,9 +1,10 @@
 <?php
 /* Database connection settings */
-	$servername = "localhost";
-    $username = "admin";		//put your phpmyadmin username.(default is "root")
-    $password = "password";			//if your phpmyadmin has a password put it here.(default is "root")
-    $dbname = "biometricattendace";
+	$servername = "sql12.freesqldatabase.com";
+    $username = "sql12792953";		//put your phpmyadmin username.(default is "root")
+    $password = "4ZIUIbdUFw";			//if your phpmyadmin has a password put it here.(default is "root")
+    $dbname = "sql12792953";
+
     
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 	
