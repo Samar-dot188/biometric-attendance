02bb25a3343b7d03034b78eeffc552453b20ed29
Samar-dot188 <gosaliasamar@gmail.com>
2025-08-10 10:53:59 +0530

    Update connectDB.php

--- public/connectDB.php
+++ public/connectDB.php
@@ -1,9 +1,9 @@
 <?php
 /* Database connection settings */
-	$servername = "sql12.freesqldatabase.com";
-    $username = "sql12792953";		//put your phpmyadmin username.(default is "root")
-    $password = "4ZIUIbdUFw";			//if your phpmyadmin has a password put it here.(default is "root")
-    $dbname = "sql12792953";
+	$servername = "biometric-attendance-gosaliasamar-856e.d.aivencloud.com";
+    $username = "avnadmin";		//put your phpmyadmin username.(default is "root")
+    $password = "AVNS_0ayC5qViELEnK-G8qRN";			//if your phpmyadmin has a password put it here.(default is "root")
+    $dbname = "defaultdb";
 
     
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
